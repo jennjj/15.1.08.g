5dcd4ab05a2ca4882c2088f23a39e28891ab33cd
jennjj <111320885+jennjj@users.noreply.github.com>
2022-08-15 17:48:03 -0500

    Add files via upload

---
+++ aviso-cookies.js
@@ -0,0 +1,20 @@
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const avisoCookies = document.getElementById('aviso-cookies');
+
+
+if(!localStorage.getItem('cookies-aceptadas')){
+    avisoCookies.classList.add('activo');
+   
+
+}
+
+
+
+botonAceptarCookies.addEventListener('click', () => {
+    avisoCookies.classList.remove('activo');
+   
+
+    localStorage.setItem('cookies-aceptadas',true);
+
+
+});
